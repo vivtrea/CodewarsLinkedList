50e41c533825518a782fc9e8e45de78be3ab5807
Andrii Hovorov <hovorov.pn@ucu.edu.ua>
2025-03-30 18:19:35 +0300

    Restoring2

---
+++ Linked Lists - Move Node/move_node.py
@@ -0,0 +1,22 @@
+"""Module provides moving node"""
+
+class Node(object):
+    '''Node class'''
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+class Context(object):
+    '''Contains source and destination'''
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source: 'Node', dest:'Node'):
+    """Moves node"""
+    head_d = source.data
+    new_dest = Node(head_d)
+    new_dest.next = dest
+
+    new_source = source.next
+    return Context(new_source, new_dest)
