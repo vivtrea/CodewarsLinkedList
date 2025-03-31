32b30fe2e377573073f8e84a0585d9047f85ecd7
Andrii Hovorov <hovorov.pn@ucu.edu.ua>
2025-03-31 12:20:07 +0300

    Loop detecion final

---
+++ Can you get the loop ?/loop_length.py
@@ -0,0 +1,30 @@
+'''Module provides loop detection'''
+
+class Node:
+    '''Node class'''
+    def __init__(self, next=None):
+        self.next = next
+
+def loop_size(node):
+    '''Detect loop and its length'''
+    slow = node
+    fast = node
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            slow = node
+            while fast is not None and fast.next is not None:
+                slow = slow.next
+                fast = fast.next
+                if slow == fast:
+                    break
+            break
+    current = slow.next
+    l = 1
+    while current != slow:
+        current = current.next
+        l += 1
+    return l
